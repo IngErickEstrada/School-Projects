a5a4e424cc7be9b595d987db79fa9e15a3fd86de
IngErickEstrada <84646084+IngErickEstrada@users.noreply.github.com>
2021-06-13 20:49:58 -0500

    Update ProyectoServidor.java

--- Parallel Computing Project/ProyectoServidor.java
+++ Parallel Computing Project/ProyectoServidor.java
@@ -1,9 +1,3 @@
-/*
-    Erick Antonio Estrada Vargas 17110079 7°M
-    Computacion Paralela
-    Proyecto Tercer Parcial
-*/
-
 public class ProyectoServidor{
 	public static void main (String[] args) {
     	ProyectoServidorWindow server = new ProyectoServidorWindow();
