9909cb5d4cc03fd8a164071e0147a2db8232b003
IngErickEstrada <84646084+IngErickEstrada@users.noreply.github.com>
2021-06-13 20:48:31 -0500

    Update ProyectoCliente.java

--- Parallel Computing Project/ProyectoCliente.java
+++ Parallel Computing Project/ProyectoCliente.java
@@ -1,9 +1,3 @@
-/*
-	Erick Antonio Estrada Vargas 17110079 7°M
-	Computacion Paralela
-	Proyecto Tercer Parcial
-*/
-	
 public class ProyectoCliente{
 	public static void main(String[] args) {
 		ProyectoClienteWindow window = new ProyectoClienteWindow();
